b5f2a552363be927b77783d60457bb028307ff29
Chribtoper <xcstolfax@gmail.com>
2018-08-02 22:57:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,10 @@ def oxford_comma(array)
 last_ele = array.last
 array.pop
 array.insert(array.size, "and #{last_ele}")
+if array.size == 2
+  newString = array.join (" ")
+else
 newString = array.join(", ")
+end
 newString
 end
